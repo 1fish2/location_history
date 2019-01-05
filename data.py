648873b2ae89a5271a68d308695095a4e62ab1fd
Jerry Morrison <1fish2@users.noreply.github.com>
2019-01-04 21:12:50 -0800

    compute segment distances, times, and speeds
    * Use geopy `great_circle()` instead of `distance()` since it's faster.
    * Restructure as a class.

--- data.py
+++ data.py
@@ -26,31 +26,26 @@ import numpy as np
 import os
 
 
-location_dtype = [
-    ('latitudeE7', 'f8'),
-    ('longitudeE7', 'f8'),
+location_dtype = [  # array internalized from the input data
+    ('latitude', 'f8'),
+    ('longitude', 'f8'),
     ('timestampMs', 'i8'),
     # altitude, accuracy, velocity, verticalAccuracy, activity
 ]
 
+delta_dtype = [('miles', 'f8'), ('seconds', 'f8'), ('mph', 'f8')]
+
 GEOCODER = geopy.Nominatim(user_agent='location distance histogram')
 
 
-def read_history():
-    """Read the decompressed Location History JSON file. Return a list[dict]."""
-    home = os.environ['HOME']
-    path = os.path.join(home, 'Downloads', 'Takeout', 'Location History', 'Location History.json')
-    with open(path, 'r') as f:
-        history = json.load(f)
-    return history['locations']
-
-
-def dicts_to_array(locations):
-    """Convert the location history from a list[dict] to a structured numpy array."""
+def dicts_to_array(history):
+    """Convert the read-in location history to a structured numpy array."""
+    locations = history['locations']  # list[dict]
     length = len(locations)
     array = np.zeros(length, dtype=location_dtype)
+
     for i, location in enumerate(locations):
-        reverse_index = length - i - 1  # the input is in reverse time order
+        reverse_index = length - i - 1  # reverse the data to forwards time order
         array[reverse_index] = (
             int(location['latitudeE7']) * 1e-7,
             int(location['longitudeE7']) * 1e-7,
@@ -59,20 +54,33 @@ def dicts_to_array(locations):
     return array
 
 
-def read_history_to_array():
-    return dicts_to_array(read_history())
-
-
 def distance_miles(loc1, loc2):
     """Return the geo distance between two locations in miles.
-    Each argument can be a (latitude, longitude) pair or an element of a history array (which prints like a triple but
-    has type numpy.void).
+    Each argument can be a (latitude, longitude) pair or an element of a location array (which prints like a tuple but
+    is really a numpy view).
     """
-    # gp.distance() and geopy.Point() accept
+    # gd.great_circle() and geopy.Point() accept
     #   latitude: float latitude in degrees (default 0)
     #   longitude: float longitude in degrees (default 0)
     #   altitude: altitude in km (default 0)
-    return gd.distance((loc1[0], loc1[1]), (loc2[0], loc2[1])).miles
+    # gp.distance() is 7x slower but slightly more accurate.
+    return gd.great_circle((loc1[0], loc1[1]), (loc2[0], loc2[1])).miles
+
+
+def compute_deltas(locations):
+    """Compute the delta miles, delta seconds, and MPH array from a locations array."""
+    deltas = np.zeros(len(locations), dtype=delta_dtype)
+    previous = locations[0]
+
+    for i in range(1, len(locations)):
+        location = locations[i]
+        miles = distance_miles(location, previous)
+        seconds = (location['timestampMs'] - previous['timestampMs']) * 1e-3
+        mph = miles / seconds * 3600.0
+        deltas[i] = (miles, seconds, mph)
+        previous = location
+
+    return deltas
 
 
 def geocode(query):
@@ -82,6 +90,24 @@ def geocode(query):
     """
     location = GEOCODER.geocode(query)
     # Location props: address, altitude, latitude, longitude, point, raw.
-    # location[0] is the formatted location string; location[1] is the (latitude, longitude) pair.
+    # location[0] is the formatted location string.
+    # location[1] is the (latitude, longitude) pair.
     return location[1]
 
+
+class HistoryData(object):
+    def __init__(self):
+        self.locations = None
+        self.deltas = None
+
+    def read(self, path=None):
+        """Read the Location History JSON file into the `locations` and `deltas` arrays."""
+        if not path:
+            home = os.environ['HOME']
+            path = os.path.join(home, 'Downloads', 'Takeout', 'Location History', 'Location History.json')
+
+        with open(path, 'r') as f:
+            history = json.load(f)
+
+        self.locations = dicts_to_array(history)
+        self.deltas = compute_deltas(self.locations)
